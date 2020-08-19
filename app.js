4950b29e34a544cd8e0ad7a0eb54c107018b99a1
Scott Davies <scottdavies1983@gmail.com>
2020-08-19 08:12:59 +0100

    views started, main index connected to database

--- app.js
+++ app.js
@@ -6,43 +6,82 @@ const { Book } = db.models;
 const app = express();
 
 app.set("view engine", "pug");
+app.use("/static", express.static("public"));
+
+/* Handler function to wrap each route. */
+function asyncHandler(cb) {
+  return async (req, res, next) => {
+    try {
+      await cb(req, res, next);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  };
+}
 
 //routes
 
 /* Home route should redirect to the /books route.*/
-app.get("/", (req, res) => {
-  res.send("<h1>Test home</h1>");
-});
+app.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    res.redirect("/books"); //think i need to set up view templates first and then delete these res.sends, also have console.log for library items
+  })
+);
 
 /*Shows the full list of books */
-app.get("/books", (req, res) => {
-  res.send("<h1>Test books</h1>");
-});
+app.get(
+  "/books",
+  asyncHandler(async (req, res) => {
+    const books = await Book.findAll();
+    // console.log(books[0].title);
+    // console.log(books[0].author);
+    // console.log(books[0].genre);
+    // console.log(books[0].year);
+
+    res.render("index", { books: books, title: "Library Manager" });
+  })
+);
 
 /* Shows the create new book form*/
-app.get("/books/new", (req, res) => {
-  res.send("<h1>Test new books</h1>");
-});
+app.get(
+  "/books/new",
+  asyncHandler(async (req, res) => {
+    res.send("<h1>Test new books</h1>");
+  })
+);
 
 /*Posts a new book to the database. */
-app.post("/books/new", (req, res) => {
-  res.send("<h1>Test home</h1>");
-});
+app.post(
+  "/books/new",
+  asyncHandler(async (req, res) => {
+    res.send("<h1>Test home</h1>");
+  })
+);
 
 /* Shows book detail form*/
-app.get("/books/:id", (req, res) => {
-  res.send("<h1>Test books id</h1>");
-});
+app.get(
+  "/books/:id",
+  asyncHandler(async (req, res) => {
+    res.send("<h1>Test books id</h1>");
+  })
+);
 
 /* Updates book info in the database*/
-app.post("/books/:id", (req, res) => {
-  res.send("<h1>Test home</h1>");
-});
+app.post(
+  "/books/:id",
+  asyncHandler(async (req, res) => {
+    res.send("<h1>Test home</h1>");
+  })
+);
 
 /*Deletes a book. Careful, this can’t be undone. It can be helpful to create a new “test” book to test deleting. */
-app.post("/books/:id/delete", (req, res) => {
-  res.send("<h1>Test home</h1>");
-});
+app.post(
+  "/books/:id/delete",
+  asyncHandler(async (req, res) => {
+    res.send("<h1>Test home</h1>");
+  })
+);
 
 /* 
 
