48500189d6d08ffb6af33f2ed8e123ee95823fab
Scott Davies <scottdavies1983@gmail.com>
2020-08-22 23:13:59 +0100

    search and error handling in place

--- app.js
+++ app.js
@@ -25,41 +25,127 @@ function asyncHandler(cb) {
   };
 }
 
-//routes
+async function libraryIndex(page, res, next) {
+  let page_size = 5;
+  const offset = page * page_size;
 
-/* Home route should redirect to the /books route.*/
-app.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    res.redirect("/books/page/0"); //think i need to set up view templates first and then delete these res.sends, also have console.log for library items
-  })
-);
+  // if (isNaN(offset)) {
+  //   const err = new Error();
+  //   err.status = 500;
+  //   next(err);
+  // }
+  const { count, rows } = await Book.findAndCountAll({
+    where: { id: { [Op.gte]: 0 } },
+    offset: offset,
+    limit: page_size,
+    order: [
+      ["author", "ASC"],
+      ["title", "ASC"],
+    ],
+  });
 
-/*Shows the full list of books */
-app.get(
-  "/books/page/:id",
-  asyncHandler(async (req, res) => {
-    let page = req.params.id;
-    let page_size = 5;
-    const offset = page * page_size;
-    const { count, rows } = await Book.findAndCountAll({
-      where: { id: { [Op.gte]: 0 } },
-      offset: offset,
-      limit: page_size,
-      order: [
-        ["author", "ASC"],
-        ["title", "ASC"],
-      ],
-    });
-
-    let numberOfPages = count / page_size;
-
+  let numberOfPages = count / page_size;
+  if (page < numberOfPages && page >= 0) {
     res.render("index", {
       books: rows,
       title: "Library Manager",
       pages: numberOfPages,
       currentPage: page,
     });
+  } else {
+    console.log("error index");
+    console.log(page);
+    console.log(numberOfPages);
+    const err = new Error();
+    console.log(err);
+    err.status = 500;
+    next(err);
+  }
+}
+
+async function search(page, query, res, next) {
+  let page_size = 5;
+  const offset = page * page_size;
+
+  const { count, rows } = await Book.findAndCountAll({
+    where: {
+      [Op.or]: [
+        {
+          title: { [Op.substring]: query },
+        },
+        {
+          author: { [Op.substring]: query },
+        },
+        {
+          genre: { [Op.substring]: query },
+        },
+        {
+          year: { [Op.substring]: query },
+        },
+      ],
+    },
+    offset: offset,
+    limit: page_size,
+    order: [
+      ["author", "ASC"],
+      ["title", "ASC"],
+    ],
+  });
+  console.log(count);
+  if (count === 0) {
+    res.render("empty-search");
+  } else {
+    let numberOfPages = count / page_size;
+
+    if (page < numberOfPages && page >= 0) {
+      res.render("search", {
+        books: rows,
+        title: "Library Manager",
+        pages: numberOfPages,
+        currentPage: page,
+        searchquery: query,
+      });
+    } else {
+      console.log("error search");
+      console.log(page);
+      console.log(numberOfPages);
+      const err = new Error();
+      console.log(err);
+      err.status = 500;
+      next(err);
+    }
+  }
+}
+
+//routes
+
+/* Home route should redirect to the /books route.*/
+app.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    res.redirect("/books/page/1"); //think i need to set up view templates first and then delete these res.sends, also have console.log for library items
+  })
+);
+
+/*Shows the full list of books */
+app.get(
+  "/books/page/:id",
+  asyncHandler(async (req, res, next) => {
+    let page = req.params.id - 1;
+    let query = req.query.searchquery;
+
+    console.log(!isNaN(page));
+    if (!isNaN(page)) {
+      if (query === undefined || query === " ") {
+        await libraryIndex(page, res, next);
+      } else {
+        await search(page, query, res, next);
+      }
+    } else {
+      const err = new Error();
+      err.status = 500;
+      next(err);
+    }
   })
 );
 
@@ -115,7 +201,7 @@ app.post(
 
     await books.update(req.body);
     console.log(req.body);
-    res.redirect("/books/");
+    res.redirect("/books/page/1");
   })
 );
 
@@ -143,7 +229,7 @@ app.use((err, req, res, next) => {
     res.status(404).render("page-not-found", { err });
   } else {
     err.message =
-      "It looks like something went wrong or the book you are looking for does not exist";
+      "It looks like something went wrong or the resource you are looking for does not exist";
     res.status(err.status || 500).render("error", { err });
   }
 });
